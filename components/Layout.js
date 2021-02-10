7649027e13ba8bd2e83b7c47e248e1364704026c
Roman Torres <rtorres@envivent.com>
2021-02-10 15:57:34 -0800

    added isi component

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Nav from "./Nav"
 import Header from "./Header"
+import Isi from "./Isi"
 import Footer from "./Footer"
 
 import styles from '../styles/Layout.module.css'
@@ -7,14 +8,15 @@ import styles from '../styles/Layout.module.css'
 const Layout = ({children}) => {
     return (
         <>
-        <div className={styles.container}>
-            <Header />
-            <Nav />
-            <main className={styles.main}>
-                {children}
-            </main>
-            <Footer />
-        </div>
+            <div className={styles.container}>
+                <Header />
+                <Nav />
+                <main className={styles.main}>
+                    {children}
+                </main>
+                <Isi />
+                <Footer />
+            </div>
         </>
     )
 }
